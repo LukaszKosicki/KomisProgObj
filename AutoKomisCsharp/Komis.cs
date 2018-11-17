05a88982193eaf94b9577f516751e241b81536d0
Lukasz Kosicki <lukasz.kosicki91@gmail.com>
2018-11-17 15:42:35 +0100

    Z menu zrobione jest pokaz pojazdy

---
+++ AutoKomisCsharp/Komis.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoKomisCsharp
+{
+    static class Komis
+    {
+        public static List<Pojazd> Pojazdy;
+        public static void Menu()
+        {
+            Pojazdy = DanePoczatkowe.BazaPojazdow;
+            Console.WriteLine("Szrot Samochodów Pożądanych");
+            Console.WriteLine("Menu");
+            Console.WriteLine("1 - Wyświetl listę pojazdów");
+            int wybor = 99;
+            while (wybor != 0)
+            {
+                Console.Write("Panie.... ? Wybierz numer z opcji.");
+                wybor = int.Parse(Console.ReadLine());
+
+                switch (wybor)
+                {
+                    case 1:
+                        WyswietlPojazdy();
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+            
+        }
+
+        public static void WyswietlPojazdy()
+        {
+            Console.WriteLine("Pojazdy w bazie");
+            foreach (Pojazd p in Pojazdy)
+            {
+                Console.WriteLine(p.wyswietlMarke());
+                Console.WriteLine(p.wyswietlModel());
+                Console.WriteLine();
+            }
+            Console.WriteLine("Aby powrócić do menu kliknij Pan dowolny klawisz.");
+            Console.ReadKey();
+        }
+    }
+}
--- AutoKomisCsharp/Program.cs
+++ AutoKomisCsharp/Program.cs
@@ -11,30 +11,7 @@ namespace AutoKomisCsharp
         
         static void Main(string[] args)
         {
-            List<Pojazd> Pojazdy = DanePoczatkowe.BazaPojazdow;
-            Console.WriteLine("Szrot Samochodów Pożądanych");
-            Console.WriteLine("Menu");
-            Console.WriteLine("1 - Wyświetl listę pojazdów");
-
-            Console.Write("Panie.... ? Wybierz numer z opcji.");
-            int wybor = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Pojazdy w bazie");
-            switch (wybor)
-            {
-                case 1:
-                    foreach(Pojazd p in Pojazdy)
-                    {
-                        Console.WriteLine(p.wyswietlMarke());
-                        Console.WriteLine(p.wyswietlModel());
-                        Console.WriteLine();
-                    }
-                    break;
-
-                    
-            }
-            Console.ReadKey();
-
+            Komis.Menu();
         }
     }
 }
